92584b68d092e7f9efac50929c076267b9c381a5
dwusiq <dwusiq@163.com>
2019-04-30 18:10:05 +0800

    fix： five seconds between two node state judgments

--- src/main/java/com/webank/webase/node/mgr/scheduler/ResetGroupListTask.java
+++ src/main/java/com/webank/webase/node/mgr/scheduler/ResetGroupListTask.java
@@ -14,12 +14,7 @@
 package com.webank.webase.node.mgr.scheduler;
 
 
-import com.webank.webase.node.mgr.front.FrontService;
-import com.webank.webase.node.mgr.frontgroupmap.FrontGroupMapService;
-import com.webank.webase.node.mgr.frontgroupmap.entity.FrontGroupMapCache;
-import com.webank.webase.node.mgr.frontinterface.FrontInterfaceService;
 import com.webank.webase.node.mgr.group.GroupService;
-import com.webank.webase.node.mgr.node.NodeService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -30,17 +25,7 @@ import org.springframework.stereotype.Component;
 public class ResetGroupListTask {
 
     @Autowired
-    private FrontService frontService;
-    @Autowired
     private GroupService groupService;
-    @Autowired
-    private NodeService nodeService;
-    @Autowired
-    private FrontGroupMapService frontGroupMapService;
-    @Autowired
-    private FrontInterfaceService frontInterfacee;
-    @Autowired
-    private FrontGroupMapCache frontGroupMapCache;
 
     /**
      * async reset groupList.
@@ -56,72 +41,4 @@ public class ResetGroupListTask {
     public void resetGroupList() {
         groupService.resetGroupList();
     }
-
-
-    /*  *//**
-     * reset groupList.
-     *//*
-    public void resetGroupList() {
-        Instant startTime = Instant.now();
-        log.info("start resetGroupList. startTime:{}", startTime.toEpochMilli());
-
-        //get all front
-        List<TbFront> frontList = frontService.getFrontList(new FrontParam());
-        if (frontList == null || frontList.size() == 0) {
-            log.info("resetGroupList jump over. front list is null");
-            return;
-        }
-        //get group from chain
-        for (TbFront front : frontList) {
-            //query group list
-            List<String> groupIdList;
-            try {
-                groupIdList = frontInterfacee
-                    .getGroupListFromSpecificFront(front.getFrontIp(), front.getFrontPort());
-            } catch (Exception ex) {
-                log.error("fail getGroupListFromSpecificFront.", ex);
-                continue;
-            }
-            for (String groupId : groupIdList) {
-                Integer gId = Integer.valueOf(groupId);
-                //peer in group
-                List<String> groupPeerList = frontInterfacee.getGroupPeers(gId);
-                //save groupId
-                groupService.saveGroupId(gId, groupPeerList.size());
-                frontGroupMapService.newFrontGroup(front.getFrontId(), gId);
-                //save new peers
-                savePeerList(gId, groupPeerList);
-                //check node status
-                nodeService.checkNodeStatus(gId);
-            }
-        }
-
-        //clear cache
-        frontGroupMapCache.clearMapList();
-
-        log.info("end resetGroupList. useTime:{} ",
-            Duration.between(startTime, Instant.now()).toMillis());
-    }
-
-    *//**
-     * save new peers.
-     *//*
-    private void savePeerList(int groupId, List<String> groupPeerList) {
-        //get all local nodes
-        List<TbNode> localNodeList = nodeService.queryByGroupId(groupId);
-        //get peers on chain
-        PeerInfo[] peerArr = frontInterfacee.getPeers(groupId);
-        List<PeerInfo> peerList = Arrays.asList(peerArr);
-        //save new nodes
-        for (String nodeId : groupPeerList) {
-            long count = localNodeList.stream().filter(
-                ln -> groupId == ln.getGroupId() && nodeId.equals(ln.getNodeId())).count();
-            if (count == 0) {
-                PeerInfo newPeer = peerList.stream().filter(peer -> nodeId.equals(peer.getNodeId()))
-                    .findFirst().orElseGet(() -> new PeerInfo(nodeId));
-                nodeService.addNodeInfo(groupId, newPeer);
-            }
-        }
-    }*/
-
 }
