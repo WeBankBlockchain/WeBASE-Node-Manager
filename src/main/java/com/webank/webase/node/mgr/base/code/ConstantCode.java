0591ceae8716b3644dfb529fbfd384b383a2bb6f
CodingCattwo <847701726@qq.com>
2019-09-27 11:44:34 +0800

    store crt's text with head&tail, fix load cert error

--- src/main/java/com/webank/webase/node/mgr/base/code/ConstantCode.java
+++ src/main/java/com/webank/webase/node/mgr/base/code/ConstantCode.java
@@ -117,7 +117,7 @@ public class ConstantCode {
     // 证书管理
     public static final RetCode CERT_ERROR = RetCode.mark(202060, "cert handle error");
     public static final RetCode FAIL_SAVE_CERT_ERROR = RetCode.mark(202061, "store cert error");
-
+    public static final RetCode CERT_FORMAT_ERROR = RetCode.mark(202060, "cert format error, must start with -----BEGIN CERTIFICATE-----\\n, end with end");
     /* auth */
     public static final RetCode USER_NOT_LOGGED_IN = RetCode.mark(302000, "user not logged in");
     public static final RetCode ACCESS_DENIED = RetCode.mark(302001, "access denied");
--- src/main/java/com/webank/webase/node/mgr/cert/CertTools.java
+++ src/main/java/com/webank/webase/node/mgr/cert/CertTools.java
@@ -1,6 +1,8 @@
 package com.webank.webase.node.mgr.cert;
 
 
+import com.webank.webase.node.mgr.base.code.ConstantCode;
+import com.webank.webase.node.mgr.base.exception.NodeMgrException;
 import com.webank.webase.node.mgr.base.tools.Web3Tools;
 import org.springframework.core.io.ClassPathResource;
 import sun.security.ec.ECPublicKeyImpl;
@@ -9,6 +11,7 @@ import sun.security.x509.X509CertImpl;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 import java.security.PublicKey;
 import java.security.cert.CertificateException;
@@ -22,8 +25,12 @@ public class CertTools {
     private final static String flag = "-----" ;
     private final static String head = "-----BEGIN CERTIFICATE-----\n" ;
     private final static String tail = "-----END CERTIFICATE-----\n" ;
+    private final static String tailForConcat = "\n-----END CERTIFICATE-----\n" ;
 
-    private static byte[] isStore = null;
+    public final static String TYPE_CHAIN = "chain";
+    public final static String TYPE_AGENCY = "agency";
+    public final static String TYPE_NODE = "node";
+
     /**
      * 获取证书类型 和 名字
      * @return
@@ -34,6 +41,17 @@ public class CertTools {
     }
     public static String  getCertName(Principal subjectDN) {
         return subjectDN.toString().split(",")[2].split("=")[1];
+    }
+
+    /**
+     * 给cert的内容加上头和尾
+     * begin ...
+     * end ...
+     */
+    public static String addHeadAndTail(String certContent) {
+        String headToConcat = head;
+        String fullCert = headToConcat.concat(certContent).concat(tailForConcat);
+        return fullCert;
     }
 
     /**
@@ -54,17 +72,46 @@ public class CertTools {
         return Web3Tools.getAddressByPublicKey(publicKey);
     }
 
+    /**
+     * 解析is获取证书list
+     * @return
+     * @throws IOException
+     */
+    public static List<X509CertImpl> getCerts(String crtContent) throws IOException, CertificateException {
+        InputStream is = new ByteArrayInputStream(crtContent.getBytes());
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        List<X509CertImpl> certs = (List<X509CertImpl>) cf.generateCertificates(is);
+        is.close();
+        return certs;
+    }
 
+    public static X509CertImpl getCert(String crtContent) throws IOException, CertificateException {
+        InputStream is = new ByteArrayInputStream(crtContent.getBytes());
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509CertImpl cert = (X509CertImpl) cf.generateCertificate(is);
+        is.close();
+        return cert;
+    }
 
-//    public static InputStream getStream(String string) throws IOException {
-//        InputStream is = new ByteArrayInputStream(string.getBytes());
-//        isStore = saveInputStream(is);
-//        return is;
-//    }
+    public static String getString(InputStream inputStream) throws IOException {
+        byte[] bytes = new byte[0];
+        bytes = new byte[inputStream.available()];
+        inputStream.read(bytes);
+        String str = new String(bytes);
+        return str;
+    }
+
+    public static InputStream getStream(String string) throws IOException {
+        InputStream is = new ByteArrayInputStream(string.getBytes());
+        return is;
+    }
 
     // 0 is node ca, 1 is agency ca, 2 is chain
     public static List<String> getSingleCrtContent(String certContent) throws IOException {
         List<String> list = new ArrayList<>();
+        if(!certContent.startsWith(head)){
+            throw new NodeMgrException(ConstantCode.CERT_FORMAT_ERROR);
+        }
         String[] nodeCrtStrArray = certContent.split(head);
         for(int i = 0; i < nodeCrtStrArray.length; i++) {
             String[] nodeCrtStrArray2 = nodeCrtStrArray[i].split(tail);
@@ -81,5 +128,24 @@ public class CertTools {
 
     public static String formatStr(String string) {
         return string.substring(0, string.length() - 1);
+    }
+
+    public String string2Base64(String str) {
+        if(null != str){
+            Base64.Encoder encoder = Base64.getEncoder();
+            return encoder.encodeToString(str.getBytes());
+        }
+        return null;
+    }
+    public String base642Str(String str) {
+        if (null != str) {
+            Base64.Decoder decoder = Base64.getDecoder();
+            try {
+                return new String(decoder.decode(str.getBytes()), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                return null;
+            }
+        }
+        return null;
     }
 }
