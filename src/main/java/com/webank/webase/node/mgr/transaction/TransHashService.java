49512f26a4a52345553b83b1eb450fcba521c486
CodingCattwo <847701726@qq.com>
2019-12-25 17:07:44 +0800

    fix getCount nullpointer

--- src/main/java/com/webank/webase/node/mgr/transaction/TransHashService.java
+++ src/main/java/com/webank/webase/node/mgr/transaction/TransHashService.java
@@ -106,11 +106,8 @@ public class TransHashService {
         log.debug("start queryCountOfTranByMinus.");
         String tableName = TableName.TRANS.getTableName(groupId);
         try {
-            Integer count = transHashMapper.getCountByMinMax(tableName);
+            int count = transHashMapper.getCountByMinMax(tableName);
             log.info("end queryCountOfTranByMinus. count:{}",  count);
-            if(count == null) {
-                return 0;
-            }
             return count;
         } catch (BadSqlGrammarException ex) {
             // TODO v1.2.2+: if trans_number not exists, use queryCountOfTran() instead
