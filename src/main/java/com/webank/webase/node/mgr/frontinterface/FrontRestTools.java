3ea9654b125403abb4c4d8ebd3108af895385000
CodingCattwo <847701726@qq.com>
2020-02-29 16:17:10 +0800

    add event front request

--- src/main/java/com/webank/webase/node/mgr/frontinterface/FrontRestTools.java
+++ src/main/java/com/webank/webase/node/mgr/frontinterface/FrontRestTools.java
@@ -95,13 +95,17 @@ public class FrontRestTools {
     public static final String URI_CERT = "cert";
     public static final String URI_ENCRYPT_TYPE = "encrypt";
 
-    //不需要在url中包含groupId的
-    private static final List<String> URI_NOT_CONTAIN_GROUP_ID = Arrays
+    public static final String URI_CONTRACT_EVENT_INFO_LIST = "event/contractEvent/list";
+    public static final String URI_NEW_BLOCK_EVENT_INFO_LIST = "event/newBlockEvent/list";
+
+    //不需要在url的前面添加groupId的
+    private static final List<String> URI_NOT_PREPEND_GROUP_ID = Arrays
         .asList(URI_CONTRACT_DEPLOY, URI_SEND_TRANSACTION, URI_KEY_PAIR, URI_CONTRACT_SENDABI,
                 URI_PERMISSION, URI_PERMISSION_FULL_LIST, URI_CNS_LIST, URI_SYS_CONFIG_LIST,
                 URI_SYS_CONFIG, URI_CONSENSUS_LIST, URI_CONSENSUS, URI_CRUD, URI_PERMISSION_SORTED_LIST,
                 URI_PERMISSION_SORTED_FULL_LIST, URI_CERT, URI_ENCRYPT_TYPE);
 
+    public static List<String> URI_CONTAIN_GROUP_ID = new ArrayList<>();
 
     @Qualifier(value = "genericRestTemplate")
     @Autowired
@@ -127,8 +131,9 @@ public class FrontRestTools {
 
         final String tempUri = uri.contains("?") ? uri.substring(0, uri.indexOf("?")) : uri;
 
-        long count = URI_NOT_CONTAIN_GROUP_ID.stream().filter(u -> u.contains(tempUri)).count();
-        if (count > 0) {
+        long countNotAppend = URI_NOT_PREPEND_GROUP_ID.stream().filter(u -> u.contains(tempUri)).count();
+        long countNotContain = URI_CONTAIN_GROUP_ID.stream().filter(u -> u.contains(tempUri)).count();
+        if (countNotAppend > 0 || countNotContain > 0) {
             return uri;
         }
         return groupId + "/" + uri;
