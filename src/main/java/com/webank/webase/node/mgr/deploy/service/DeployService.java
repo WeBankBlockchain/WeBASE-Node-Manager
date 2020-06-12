334224fa7080082b7247a6ba69412048efb5f367
CodingCattwo <847701726@qq.com>
2020-06-12 16:00:23 +0800

    add map status when new

--- src/main/java/com/webank/webase/node/mgr/deploy/service/DeployService.java
+++ src/main/java/com/webank/webase/node/mgr/deploy/service/DeployService.java
@@ -220,7 +220,7 @@ public class DeployService {
                                 groupId, ip, nodeConfig.getP2pPort(),
                                 nodeName, NodeStatusEnum.DEAD);
 
-                        this.frontGroupMapService.newFrontGroup(front.getFrontId(), groupId);
+                        this.frontGroupMapService.newFrontGroup(front.getFrontId(), groupId, GroupStatus.MAINTAINING);
                     });
 
                     // generate front application.yml
@@ -404,7 +404,7 @@ public class DeployService {
      * @return
      */
     @Transactional(propagation = Propagation.REQUIRED)
-    public Pair<RetCode, String> deployNode(String chainName, int groupId, String ip, String agencyName, int num) {
+    public Pair<RetCode, String> deployNodes(String chainName, int groupId, String ip, String agencyName, int num) {
         log.info("Check chain name:[{}] exists...", chainName);
         TbChain chain = tbChainMapper.getByChainName(chainName);
         if (chain == null) {
@@ -416,6 +416,7 @@ public class DeployService {
             throw new NodeMgrException(ConstantCode.IP_FORMAT_ERROR);
         }
 
+        // TODO get proper node number according to host performance
         log.info("Check num:[{}]...", num);
         if (num <= 0 || num >= 200) {
             throw new NodeMgrException(ConstantCode.NODES_NUM_ERROR);
@@ -461,7 +462,7 @@ public class DeployService {
 
             // generate config files and scp to remote
             this.nodeService.generateNodeConfig(chain, groupId, ip, newFrontList);
-
+            // TODO update group's other node's config.ini p2p list
             // generate config files and scp to remote
             this.groupService.generateGroupConfigsAndScp(newGroup, chain, groupId, ip, newFrontList);
 
@@ -478,6 +479,21 @@ public class DeployService {
         }
 
         return Pair.of(ConstantCode.SUCCESS, "success");
+    }
+
+
+    public void deleteNode() {
+        // check node is removed from sealer/observer, else abort
+
+        // remove front
+
+        // destroy docker-container
+
+        // update local group's nodes' config.ini p2p, mv to temp dir
+
+        // update host nodes' config.ini
+
+        // mv host node's data file
     }
 }
 
