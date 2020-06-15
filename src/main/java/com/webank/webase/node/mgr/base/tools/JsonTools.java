8d95c96c5d8ff1cb00073292021ceb676d3b34f2
CodingCattwo <847701726@qq.com>
2020-06-15 19:26:47 +0800

    rm code in json util

--- src/main/java/com/webank/webase/node/mgr/base/tools/JsonTools.java
+++ src/main/java/com/webank/webase/node/mgr/base/tools/JsonTools.java
@@ -38,10 +38,6 @@ public class JsonTools {
         mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         // 允许key有单引号
         mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-        // 允许整数以0开头
-        mapper.configure(JsonParser.Feature.ALLOW_NUMERIC_LEADING_ZEROS, true);
-        // 允许字符串中存在回车换行控制符
-        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
         // 属性值为null的不参与序列化
 //        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
