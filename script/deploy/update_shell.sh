b37dd3bb8b212e81ab66d6391d671a1129da0fbb
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-07-27 15:21:13 +0800

    add 2.5.0 support

--- script/deploy/update_shell.sh
+++ script/deploy/update_shell.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/v2.4.1/tools/build_chain.sh > build_chain.sh
-curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/v2.4.1/tools/gen_agency_cert.sh > gen_agency_cert.sh
-curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/v2.4.1/tools/gen_node_cert.sh > gen_node_cert.sh
+curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/v2.5.0/tools/build_chain.sh > build_chain.sh
+curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/v2.5.0/tools/gen_agency_cert.sh > gen_agency_cert.sh
+curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/v2.5.0/tools/gen_node_cert.sh > gen_node_cert.sh
 
 # delete pwd
 row=$(grep -i  -n output_dir build_chain.sh |grep -i  "pwd"| awk -F ":" '{print $1}')
