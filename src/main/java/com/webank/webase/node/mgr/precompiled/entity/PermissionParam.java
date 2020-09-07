0b6f5e2a5021568aa00ea1c8f1fdbf925729b3fc
CodingCattwo <847701726@qq.com>
2020-09-07 11:50:31 +0800

    add chain governannce api

--- src/main/java/com/webank/webase/node/mgr/precompiled/entity/PermissionParam.java
+++ src/main/java/com/webank/webase/node/mgr/precompiled/entity/PermissionParam.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.webank.webase.node.mgr.precompiled.permission;
+package com.webank.webase.node.mgr.precompiled.entity;
 
 
 import lombok.Data;
