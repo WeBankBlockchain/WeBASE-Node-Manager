44fb92f31886883426616381593fa5b8f9bd16af
CodingCattwo <847701726@qq.com>
2020-09-08 18:24:33 +0800

    add save voteRecord&statusRecord

--- src/main/java/com/webank/webase/node/mgr/governance/ContractStatusService.java
+++ src/main/java/com/webank/webase/node/mgr/governance/ContractStatusService.java
@@ -15,20 +15,46 @@
 package com.webank.webase.node.mgr.governance;
 
 import com.webank.webase.node.mgr.base.code.ConstantCode;
+import com.webank.webase.node.mgr.base.enums.FreezeStatus;
 import com.webank.webase.node.mgr.base.exception.NodeMgrException;
 import com.webank.webase.node.mgr.governance.entity.GovernParam;
 import com.webank.webase.node.mgr.governance.entity.TbContractStatus;
+import com.webank.webase.node.mgr.precompiled.entity.ContractStatusHandle;
 import java.util.List;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * contract freeze status record
  */
+@Log4j2
 @Service
 public class ContractStatusService {
     @Autowired
     private ContractStatusMapper statusMapper;
+
+    // contract manage type
+    private static final String CONTRACT_MANAGE_FREEZE = "freeze";
+
+    /**
+     * when freeze contract successfully, save
+     */
+    public void saveContractStatus(ContractStatusHandle statusHandle) {
+        log.debug("saveContractStatus statusHandle:{}", statusHandle);
+        TbContractStatus tbContractStatus = new TbContractStatus();
+        tbContractStatus.setGroupId(statusHandle.getGroupId());
+        tbContractStatus.setContractAddress(statusHandle.getContractAddress());
+        tbContractStatus.setModifyAddress(statusHandle.getFromAddress());
+        // status
+        tbContractStatus.setStatus(FreezeStatus.NORMAL.getValue());
+        if (CONTRACT_MANAGE_FREEZE.equals(statusHandle.getHandleType())) {
+            tbContractStatus.setStatus(FreezeStatus.FROZEN.getValue());
+        }
+        statusMapper.add(tbContractStatus);
+        log.debug("saveContractStatus tbContractStatus:{}", tbContractStatus);
+
+    }
 
     public List<TbContractStatus> getStatusList(GovernParam governParam) {
         return statusMapper.getList(governParam);
