2c788761f8e4c16f7a581b5797f7cda82e485fc4
CodingCattwo <847701726@qq.com>
2020-10-29 17:48:43 +0800

    add save path api

--- src/main/java/com/webank/webase/node/mgr/contract/ContractController.java
+++ src/main/java/com/webank/webase/node/mgr/contract/ContractController.java
@@ -13,7 +13,6 @@
  */
 package com.webank.webase.node.mgr.contract;
 
-import com.webank.webase.node.mgr.base.tools.JsonTools;
 import com.webank.webase.node.mgr.base.code.ConstantCode;
 import com.webank.webase.node.mgr.base.controller.BaseController;
 import com.webank.webase.node.mgr.base.entity.BasePageResponse;
@@ -21,13 +20,13 @@ import com.webank.webase.node.mgr.base.entity.BaseResponse;
 import com.webank.webase.node.mgr.base.enums.SqlSortType;
 import com.webank.webase.node.mgr.base.exception.NodeMgrException;
 import com.webank.webase.node.mgr.base.properties.ConstantProperties;
+import com.webank.webase.node.mgr.base.tools.JsonTools;
 import com.webank.webase.node.mgr.contract.entity.Contract;
 import com.webank.webase.node.mgr.contract.entity.ContractParam;
 import com.webank.webase.node.mgr.contract.entity.ContractPathParam;
 import com.webank.webase.node.mgr.contract.entity.DeployInputParam;
 import com.webank.webase.node.mgr.contract.entity.QueryByBinParam;
 import com.webank.webase.node.mgr.contract.entity.QueryContractParam;
-import com.webank.webase.node.mgr.contract.entity.RspContractPath;
 import com.webank.webase.node.mgr.contract.entity.TbContract;
 import com.webank.webase.node.mgr.contract.entity.TbContractPath;
 import com.webank.webase.node.mgr.contract.entity.TransactionInputParam;
@@ -48,7 +47,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
@@ -58,6 +56,8 @@ public class ContractController extends BaseController {
 
     @Autowired
     private ContractService contractService;
+    @Autowired
+    private ContractPathService contractPathService;
 
     /**
      * add new contract info.
@@ -226,9 +226,29 @@ public class ContractController extends BaseController {
     }
 
     /**
+     * add contract path
+     */
+    @PostMapping(value = "/contractPath")
+    @PreAuthorize(ConstantProperties.HAS_ROLE_ADMIN_OR_DEVELOPER)
+    public BaseResponse addContractPath(@Valid @RequestBody ContractPathParam param) {
+        BaseResponse response = new BaseResponse(ConstantCode.SUCCESS);
+        Instant startTime = Instant.now();
+        log.info("start addContractPath. startTime:{} param:{}",
+            startTime.toEpochMilli(), param);
+
+        int result = contractPathService.save(param.getGroupId(), param.getContractPath());
+        response.setData(result);
+
+        log.info("end addContractPath. useTime:{} add result:{}",
+            Duration.between(startTime, Instant.now()).toMillis(), result);
+        return response;
+    }
+
+
+    /**
      * qurey contract info list.
      */
-    @PostMapping(value = "/contractPathList/{groupId}")
+    @PostMapping(value = "/contractPath/list/{groupId}")
     public BasePageResponse queryContractPathList(@PathVariable("groupId") Integer groupId) {
         BasePageResponse pagesponse = new BasePageResponse(ConstantCode.SUCCESS);
         Instant startTime = Instant.now();
