f17ee3a1f2d737601e450ae7cbca74a53ca3e249
CodingCattwo <847701726@qq.com>
2020-12-15 17:40:02 +0800

    add docker shelle service

--- src/main/java/com/webank/webase/node/mgr/base/code/ConstantCode.java
+++ src/main/java/com/webank/webase/node/mgr/base/code/ConstantCode.java
@@ -250,6 +250,7 @@ public class ConstantCode {
     public static final RetCode NODES_NUM_EXCEED_MAX_ERROR = RetCode.mark(202467, "Max 4 nodes on a same host.");
     public static final RetCode SAME_HOST_ERROR = RetCode.mark(202468, "Cannot install node and WeBASE-Node-Manager on same host.");
     // add in 1.4.3
+    public static final RetCode EXEC_DOCKER_CHECK_SCRIPT_ERROR = RetCode.mark(202469, "Check docker installed and running of host");
     public static final RetCode EXEC_HOST_CHECK_SCRIPT_ERROR_FOR_MEM = RetCode.mark(202470, "Check host memory not enough for nodes(s)");
     public static final RetCode EXEC_HOST_CHECK_SCRIPT_ERROR_FOR_CPU = RetCode.mark(202471, "Check host cpu core count not enough for node(s)");
 
