0a76f728760a7d81adce3b95e78e5843ce580b47
CodingCattwo <847701726@qq.com>
2021-03-09 17:06:06 +0800

    add external data pull from block

---
+++ src/main/java/com/webank/webase/node/mgr/external/ExtContractService.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2014-2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.webank.webase.node.mgr.external;
+
+import com.webank.webase.node.mgr.base.code.ConstantCode;
+import com.webank.webase.node.mgr.base.exception.NodeMgrException;
+import com.webank.webase.node.mgr.base.tools.NodeMgrTools;
+import com.webank.webase.node.mgr.contract.ContractService;
+import com.webank.webase.node.mgr.contract.entity.ContractParam;
+import com.webank.webase.node.mgr.external.entity.TbExternalContract;
+import com.webank.webase.node.mgr.external.mapper.TbExternalContractMapper;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class ExtContractService {
+
+    @Autowired
+    private TbExternalContractMapper contractMapper;
+    @Autowired
+    private ContractService contractService;
+
+
+    /**
+     * save contract on chain
+     */
+    public int saveContractOnChain(int groupId, String contractAddress, String txHash,
+        String deployAddress, String timestamp) {
+        log.info("saveContractOnChain groupId:{} contractAddress:{}", groupId, contractAddress);
+        if (checkAddressExist(groupId, contractAddress)) {
+            return 0;
+        }
+        TbExternalContract tbContract = new TbExternalContract();
+        tbContract.setGroupId(groupId);
+        tbContract.setContractAddress(contractAddress);
+        tbContract.setDeployTxHash(txHash);
+        tbContract.setDeployTime(NodeMgrTools.timestamp2Date(Long.parseLong(timestamp)));
+        tbContract.setDeployAddress(deployAddress);
+        Date now = new Date();
+        tbContract.setCreateTime(now);
+        tbContract.setModifyTime(now);
+        int insertRes = contractMapper.insertSelective(tbContract);
+        log.info("saveContractOnChain insertRes:{}", insertRes);
+        return insertRes;
+    }
+
+    private boolean checkAddressExist(int groupId, String contractAddress) {
+        // check tb_user's address
+        ContractParam queryParam = new ContractParam();
+        queryParam.setGroupId(groupId);
+        queryParam.setContractAddress(contractAddress);
+        int countOfContract = contractService.countOfContract(queryParam);
+        if (countOfContract > 0) {
+            log.debug("saveContractOnChain exist tb_contract " 
+                + "contractAddress:{} address:{}", groupId, contractAddress);
+            return true;
+        }
+        int count = contractMapper.countOfExtContract(groupId, contractAddress);
+        if (count > 0) {
+            log.debug("saveContractOnChain exists tb_external_contract" 
+                + " groupId:{} address:{}", groupId, contractAddress);
+            return true;
+        }
+        return false;
+    }
+
+    public List<TbExternalContract> listExtContract(ContractParam param) {
+        return contractMapper.listExtContract(param);
+    }
+
+    public int countExtContract(ContractParam param) {
+        return contractMapper.countExtContract(param);
+        
+    }
+    public int updateContractInfo(int contractId, String contractName, String abi, String description) {
+        TbExternalContract update = contractMapper.selectByPrimaryKey(contractId);
+        if (update == null) {
+            log.error("updateContractInfo id not exist!");
+            throw new NodeMgrException(ConstantCode.INVALID_CONTRACT_ID);
+        }
+        update.setContractName(contractName);
+        update.setContractAbi(abi);
+        update.setDescription(description);
+        return contractMapper.updateByPrimaryKeySelective(update);
+    }
+}
