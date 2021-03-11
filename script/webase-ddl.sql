f095830011f5b9faec59c202a997a54ddb17021d
CodingCattwo <847701726@qq.com>
2021-03-11 16:01:46 +0800

    update ext contract table

--- script/webase-ddl.sql
+++ script/webase-ddl.sql
@@ -462,12 +462,12 @@ CREATE TABLE IF NOT EXISTS tb_stat (
 CREATE TABLE IF NOT EXISTS tb_external_account (
   id int(11) NOT NULL AUTO_INCREMENT COMMENT '外部账户编号',
   group_id int(11) DEFAULT NULL COMMENT '所属群组编号',
-  public_key varchar(250) NOT NULL COMMENT '公钥',
   address varchar(64) DEFAULT NULL COMMENT '在链上位置的hash',
+  public_key varchar(250) DEFAULT NULL COMMENT '公钥',
   sign_user_id varchar(64) DEFAULT NULL COMMENT '签名服务中的user的业务id',
   has_pk int(1) DEFAULT 1 COMMENT '是否拥有私钥信息(1-拥有，2-不拥有)',
   user_name varchar(64) binary DEFAULT NULL COMMENT '用户名',
-  user_status int(1) NOT NULL DEFAULT '1' COMMENT '状态（1-正常 2-停用）',
+  user_status int(1) DEFAULT NULL DEFAULT '1' COMMENT '状态（1-正常 2-停用）',
   create_time datetime DEFAULT NULL COMMENT '创建时间',
   modify_time datetime DEFAULT NULL COMMENT '修改时间',
   description varchar(250) DEFAULT NULL COMMENT '备注',
@@ -486,11 +486,11 @@ CREATE TABLE IF NOT EXISTS tb_external_account (
 CREATE TABLE IF NOT EXISTS tb_external_contract (
   id int(11) NOT NULL AUTO_INCREMENT COMMENT '外部合约编号',
   group_id int(11) NOT NULL COMMENT '所属群组编号',
-  contract_bin mediumtext COMMENT '合约链上binary',
   contract_address varchar(64) NOT NULL COMMENT '合约地址',
-  deploy_address varchar(64) DEFAULT NULL COMMENT '合约部署者地址',
-  deploy_tx_hash varchar(64) DEFAULT NULL COMMENT '合约部署的交易哈希',
-  deploy_time datetime DEFAULT NULL COMMENT '部署时间',
+  deploy_address varchar(64) NOT NULL COMMENT '合约部署者地址',
+  deploy_tx_hash varchar(120) NOT NULL COMMENT '合约部署的交易哈希',
+  deploy_time datetime NOT NULL COMMENT '部署时间',
+  contract_bin mediumtext DEFAULT NULL COMMENT '合约链上binary',
   contract_status int(1) DEFAULT '1' COMMENT '部署状态（1：未部署，2：部署成功，3：部署失败）',
   contract_type tinyint(4) DEFAULT '0' COMMENT '合约类型(0-普通合约，1-系统合约)',
   contract_name varchar(120) binary DEFAULT NULL COMMENT '合约名称',
@@ -502,6 +502,6 @@ CREATE TABLE IF NOT EXISTS tb_external_contract (
   description text COMMENT '描述',
   PRIMARY KEY (id),
   UNIQUE KEY uk_group_path_name (group_id,contract_address)
-) ENGINE=InnoDB AUTO_INCREMENT=800001 DEFAULT CHARSET=utf8 COMMENT='合约表';
+) ENGINE=InnoDB AUTO_INCREMENT=800001 DEFAULT CHARSET=utf8 COMMENT='外部合约表';
 
 SET FOREIGN_KEY_CHECKS = 1;
