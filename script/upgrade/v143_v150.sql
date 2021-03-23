e2c12d0ea955a6bb966e50412cd98b255865d812
CodingCattwo <847701726@qq.com>
2021-03-23 17:47:52 +0800

    fix alter

--- script/upgrade/v143_v150.sql
+++ script/upgrade/v143_v150.sql
@@ -1,3 +1,44 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE TABLE IF NOT EXISTS tb_app_info (
+  id int(11) NOT NULL AUTO_INCREMENT COMMENT '自增编号',
+  app_name varchar(128) NOT NULL COMMENT '应用名',
+  app_key varchar(16) NOT NULL COMMENT '应用Key',
+  app_secret varchar(32) DEFAULT NULL COMMENT '应用密码',
+  app_type int(1) NOT NULL DEFAULT '2' COMMENT '应用类型(1模板，2新建)',
+  app_status int(1) NOT NULL DEFAULT '2' COMMENT '应用状态(1存活，2不存活)',
+  app_doc_link varchar(256) DEFAULT NULL COMMENT '应用文档链接',
+  app_link varchar(256) DEFAULT NULL COMMENT '应用链接',
+  app_ip varchar(16) DEFAULT NULL COMMENT '应用ip',
+  app_port int(11) DEFAULT NULL COMMENT '应用端口',
+  app_icon mediumtext DEFAULT NULL COMMENT '应用图标',
+  app_desc varchar(1024) DEFAULT NULL COMMENT '应用描述',
+  app_detail text DEFAULT NULL COMMENT '应用详情',
+  create_time datetime DEFAULT NULL COMMENT '创建时间',
+  modify_time datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (id),
+  UNIQUE KEY uk_key (app_key),
+  KEY uk_name (app_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='应用信息表';
+
+-- ----------------------------
+-- Table structure for tb_contract_store
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS tb_contract_store (
+  id int(11) NOT NULL AUTO_INCREMENT COMMENT '自增编号',
+  app_key varchar(16) NOT NULL COMMENT '所属应用Key',
+  contract_name varchar(120) binary NOT NULL COMMENT '合约名称',
+  contract_version varchar(120) NOT NULL COMMENT '合约版本',
+  contract_source mediumtext COMMENT '合约源码',
+  contract_abi mediumtext COMMENT '编译合约生成的abi文件内容',
+  bytecode_bin mediumtext COMMENT '合约bytecodeBin',
+  account varchar(50) binary DEFAULT 'admin' COMMENT '关联账号',
+  create_time datetime DEFAULT NULL COMMENT '创建时间',
+  modify_time datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (id),
+  UNIQUE KEY uk_version (app_key,contract_name,contract_version)
+) ENGINE=InnoDB AUTO_INCREMENT=300001 DEFAULT CHARSET=utf8 COMMENT='应用合约仓库';
 
 CREATE TABLE IF NOT EXISTS tb_stat (
   id int(11) NOT NULL AUTO_INCREMENT COMMENT '统计编号',
@@ -62,5 +103,16 @@ CREATE TABLE IF NOT EXISTS tb_external_contract (
   UNIQUE KEY uk_group_path_name (group_id,contract_address)
 ) ENGINE=InnoDB AUTO_INCREMENT=800001 DEFAULT CHARSET=utf8 COMMENT='外部合约表';
 
--- rm unique key of contract_name on table tb_abi
+-- 修改表 --
 ALTER TABLE tb_abi DROP INDEX unique_name;
+ALTER TABLE tb_contract MODIFY COLUMN contract_path varchar(128) binary NOT NULL COMMENT '合约所在目录';
+ALTER TABLE tb_cns MODIFY COLUMN contract_path varchar(128) binary NOT NULL COMMENT '合约所在目录';
+ALTER TABLE tb_contract_path MODIFY COLUMN contract_path varchar(128) binary NOT NULL COMMENT '合约所在目录';
+
+
+-- 插入默认数据 --
+-- if begin end, else begin end
+INSERT INTO `tb_app_info` (`app_name`, `app_key`, `app_type`, `app_doc_link`, `app_desc`, `app_detail`, `create_time`, `modify_time`) VALUES ('WeId-temp', 'app00001', 1, 'WeIdentity是一套分布式多中心的技术解决方案，可承载实体对象（人或者物）的现实身份与链上身份的可信映射、以及实现实体对象之间安全的访问授权与数据交换。WeIdentity由微众银行自主研发并完全开源，秉承公众联盟链整合资源、交换价值、服务公众的理念，致力于成为链接多个垂直行业领域的分布式商业基础设施，促进泛行业、跨机构、跨地域间的身份认证和数据合作。', 'WeIdentity目前主要包含两大模块：WeIdentity DID以及WeIdentity Credential。分布式身份标识 (WeIdentity DID)传统方式中，用户的注册和身份管理完全依赖于单一中心的注册机构；随着分布式账本技术（例如区块链）的出现，分布式多中心的身份注册、标识和管理成为可能。 WeIdentity DID模块在FISCO-BCOS区块链底层平台上实现了一套符合W3C DID规范的分布式多中心的身份标识协议，使实体（人或物）的现实身份实现了链上的身份标识；同时，WeIdentity DID给与Entity（人或者物）直接拥有和控制自己身份ID的能力。可验证数字凭证 (WeIdentity Credential)现实世界中存在着各种各样用于描述实体身份、实体间关系的数据，如身份证、行驶证、存款证明、处方、毕业证、房产证、信用报告等。WeIdentity Credential提供了一整套基于W3C VC规范的解决方案，旨在对这一类数据进行标准化、电子化，生成可验证、可交换的「凭证」（Credential），支持对凭证的属性进行选择性披露，及生成链上存证（Evidence）', now(), now());
+
+
+SET FOREIGN_KEY_CHECKS = 1;
