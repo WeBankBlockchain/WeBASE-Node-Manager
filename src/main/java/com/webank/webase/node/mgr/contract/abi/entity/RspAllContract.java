6968f719efa55c8e6b151ec84910921863ad909d
CodingCattwo <847701726@qq.com>
2021-03-25 12:03:25 +0800

    fix abi id

--- src/main/java/com/webank/webase/node/mgr/contract/abi/entity/RspAllContract.java
+++ src/main/java/com/webank/webase/node/mgr/contract/abi/entity/RspAllContract.java
@@ -26,7 +26,6 @@ import lombok.ToString;
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class RspAllContract extends TbContract {
