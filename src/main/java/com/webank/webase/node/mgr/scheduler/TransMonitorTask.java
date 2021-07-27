db6ee2070a1af0ad984859e90f60096904cfd3b8
CodingCattwo <847701726@qq.com>
2021-07-27 11:48:00 +0800

    fix dir in zip

--- src/main/java/com/webank/webase/node/mgr/scheduler/TransMonitorTask.java
+++ src/main/java/com/webank/webase/node/mgr/scheduler/TransMonitorTask.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.webank.webase.node.mgr.config.scheduler;
+package com.webank.webase.node.mgr.scheduler;
 
 import com.webank.webase.node.mgr.base.enums.DataStatus;
 import com.webank.webase.node.mgr.group.GroupService;
