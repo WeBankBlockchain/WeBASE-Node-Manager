accbcf1bb66126b3aa57d1d847a402f0a2bd28c7
CodingCattwo <847701726@qq.com>
2021-10-18 17:41:03 +0800

    fix host check local ip

--- src/main/java/com/webank/webase/node/mgr/deploy/service/HostService.java
+++ src/main/java/com/webank/webase/node/mgr/deploy/service/HostService.java
@@ -15,6 +15,9 @@
  */
 package com.webank.webase.node.mgr.deploy.service;
 
+import static com.webank.webase.node.mgr.tools.IPUtil.LOCAL_IP_127;
+import static com.webank.webase.node.mgr.tools.IPUtil.LOCAL_IP_host;
+
 import com.webank.webase.node.mgr.base.code.ConstantCode;
 import com.webank.webase.node.mgr.base.enums.HostStatusEnum;
 import com.webank.webase.node.mgr.base.enums.ScpTypeEnum;
@@ -861,7 +864,7 @@ public class HostService {
             }
         } else {
             // check whether 127.0.0.1 in tb_host
-            TbHost hostOfLocalIp = tbHostMapper.getByIp(IPUtil.LOCAL_IP_127);
+            TbHost hostOfLocalIp = tbHostMapper.getByIp(LOCAL_IP_127);
             if (hostOfLocalIp == null) {
                 log.info("host of 127.0.0.1 not in tb_host, skip check local");
                 return;
@@ -880,7 +883,7 @@ public class HostService {
      */
     private void validateAllLocalhostOrNot(String ip) {
         log.info("check validateAllLocalhostOrNot ip:{}",ip);
-        if (IPUtil.isLocal(ip)) {
+        if (LOCAL_IP_127.equals(ip) || LOCAL_IP_host.equalsIgnoreCase(ip)) {
             // if ip is 127.0.0.1, check all other host ip
             List<TbHost> hostList = tbHostMapper.selectAll();
             // 127.0.0.1 is the first host, pass
@@ -894,7 +897,7 @@ public class HostService {
             }
         } else {
             // check whether 127.0.0.1 in tb_host
-            TbHost hostOfLocalIp = tbHostMapper.getByIp(IPUtil.LOCAL_IP_127);
+            TbHost hostOfLocalIp = tbHostMapper.getByIp(LOCAL_IP_127);
             if (hostOfLocalIp == null) {
                 log.info("host of 127.0.0.1 not in tb_host, skip check local");
                 return;
