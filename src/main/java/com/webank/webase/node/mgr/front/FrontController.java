31ad7b125a71e9d93adac4eff2787f6909c4b713
CodingCattwo <847701726@qq.com>
2021-11-29 14:47:51 +0800

    recover refresh front

--- src/main/java/com/webank/webase/node/mgr/front/FrontController.java
+++ src/main/java/com/webank/webase/node/mgr/front/FrontController.java
@@ -59,17 +59,17 @@ public class FrontController extends BaseController {
     private FrontInterfaceService frontInterfaceService;
 
     /**
-     * refresh frontn
+     * refresh front
      */
-//    @GetMapping("/refresh")
-//    public BaseResponse refreshFront() {
-//    	Instant startTime = Instant.now();
-//    	log.info("start refreshFront startTime:{}", startTime.toEpochMilli());
-//    	BaseResponse baseResponse = new BaseResponse(ConstantCode.SUCCESS);
-//    	frontService.refreshFront();
-//    	log.info("end refreshFront useTime:{}", Duration.between(startTime, Instant.now()).toMillis());
-//    	return baseResponse;
-//    }
+    @GetMapping("/refresh")
+    public BaseResponse refreshFront() {
+    	Instant startTime = Instant.now();
+    	log.info("start refreshFront startTime:{}", startTime.toEpochMilli());
+    	BaseResponse baseResponse = new BaseResponse(ConstantCode.SUCCESS);
+    	frontService.refreshFront();
+    	log.info("end refreshFront useTime:{}", Duration.between(startTime, Instant.now()).toMillis());
+    	return baseResponse;
+    }
     
     /**
      * add new front
