52a03fdb93ee2343c9736add92b42d8557ad663c
eveagle <zhongshaobo1993@gmail.com>
2021-12-03 11:25:25 +0800

    add some todo

--- src/main/java/com/webank/webase/node/mgr/config/security/filter/AppIntegrationFilter.java
+++ src/main/java/com/webank/webase/node/mgr/config/security/filter/AppIntegrationFilter.java
@@ -13,6 +13,7 @@
  */
 package com.webank.webase.node.mgr.config.security.filter;
 
+import com.webank.webase.node.mgr.appintegration.AppIntegrationService;
 import com.webank.webase.node.mgr.appintegration.entity.TbAppInfo;
 import com.webank.webase.node.mgr.base.code.ConstantCode;
 import com.webank.webase.node.mgr.base.exception.NodeMgrException;
@@ -33,6 +34,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Log4j2
 @Component
 public class AppIntegrationFilter implements HandlerInterceptor {
+
+    @Autowired
+    private AppIntegrationService appIntegrationService;
 
     @Autowired
     private ConstantProperties cproperties;
@@ -68,17 +72,16 @@ public class AppIntegrationFilter implements HandlerInterceptor {
         if (StringUtils.isBlank(signature)) {
             throw new NodeMgrException(ConstantCode.SIGNATURE_CANNOT_EMPTY);
         }
-//        TbAppInfo tbAppInfo = appIntegrationService.queryAppInfoByAppKey(appKey);
-//        if (Objects.isNull(tbAppInfo)) {
-//            throw new NodeMgrException(ConstantCode.APPKEY_NOT_EXISTS);
-//        }
+        TbAppInfo tbAppInfo = appIntegrationService.queryAppInfoByAppKey(appKey);
+        if (Objects.isNull(tbAppInfo)) {
+            throw new NodeMgrException(ConstantCode.APPKEY_NOT_EXISTS);
+        }
         long reqeustInterval = System.currentTimeMillis() - Long.valueOf(timestamp);
         if (reqeustInterval > cproperties.getAppRequestTimeOut()) {
             throw new NodeMgrException(ConstantCode.TIMESTAMP_TIMEOUT);
         }
         // joint data string to md5
-//        String dataStr = timestamp + appKey + tbAppInfo.getAppSecret();
-        String dataStr = timestamp + appKey;
+        String dataStr = timestamp + appKey + tbAppInfo.getAppSecret();
         if (!signature.equals(NodeMgrTools.md5Encrypt(dataStr))) {
             log.warn("fail validateAppRequest. signature not match.");
             throw new NodeMgrException(ConstantCode.SIGNATURE_NOT_MATCH);
