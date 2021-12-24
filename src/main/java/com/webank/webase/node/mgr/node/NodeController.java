1f481451a04de513803336aad0792d6a932287f1
eveagle <zhongshaobo1993@gmail.com>
2021-12-24 17:16:23 +0800

    rollback

--- src/main/java/com/webank/webase/node/mgr/node/NodeController.java
+++ src/main/java/com/webank/webase/node/mgr/node/NodeController.java
@@ -94,34 +94,6 @@ public class NodeController {
     }
 
     /**
-     * get node info detail
-     */
-    @GetMapping(value = "/nodeInfo/{groupId}/{nodeId}")
-    public BaseResponse getNodeInfo(@PathVariable("groupId") String groupId,
-        @PathVariable("nodeId") String nodeId)
-        throws NodeMgrException {
-
-        Instant startTime = Instant.now();
-        log.info("start getNodeInfo startTime:{} groupId:{},nodeId:{}",
-            startTime.toEpochMilli(), groupId, nodeId);
-
-        // param
-        NodeParam param = new NodeParam();
-        param.setGroupId(groupId);
-        param.setNodeId(nodeId);
-
-        // query node row
-        TbNode tbNode = nodeService.queryNodeInfo(param);
-
-        BaseResponse baseResponse = new BaseResponse(ConstantCode.SUCCESS);
-        baseResponse.setData(tbNode);
-
-        log.info("end getNodeInfo useTime:{} result:{}",
-            Duration.between(startTime, Instant.now()).toMillis(), JsonTools.toJSONString(baseResponse));
-        return baseResponse;
-    }
-
-    /**
      * get node info.
      */
     @GetMapping(value = "/nodeInfo/{groupId}")
@@ -148,6 +120,25 @@ public class NodeController {
     }
 
     /**
+     * get node id list
+     */
+    //todo check
+/*    @GetMapping("/nodeIdList/{groupId}")
+    public BaseResponse getNodeIdList(@PathVariable("groupId") String groupId) {
+        Instant startTime = Instant.now();
+        log.info("start getNodeIdList startTime:{} groupId:{}",
+                startTime.toEpochMilli(), groupId);
+        List<String> res = nodeService.getNodeIdListService(groupId);
+
+        BaseResponse baseResponse = new BaseResponse(ConstantCode.SUCCESS);
+        baseResponse.setData(res);
+
+        log.info("end getNodeIdList useTime:{} result:{}",
+                Duration.between(startTime, Instant.now()).toMillis(), JsonTools.toJSONString(baseResponse));
+        return baseResponse;
+    }*/
+
+    /**
      * update tb_node info of city, agency, ip etc.
      */
     @PutMapping("/description")
@@ -162,7 +153,7 @@ public class NodeController {
         baseResponse.setData(res);
 
         log.info("end updateDesc useTime:{} result:{}",
-                Duration.between(startTime, Instant.now()).toMillis(), JsonTools.toJSONString(baseResponse));
+            Duration.between(startTime, Instant.now()).toMillis(), JsonTools.toJSONString(baseResponse));
         return baseResponse;
     }
 
@@ -183,6 +174,4 @@ public class NodeController {
             Duration.between(startTime, Instant.now()).toMillis(), JsonTools.toJSONString(baseResponse));
         return baseResponse;
     }
-
-
 }
