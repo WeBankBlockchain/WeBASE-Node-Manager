018f1b131596514c7b015b46a3c8f7f33d6b4709
mawla <mawla@dcits.com>
2022-08-24 18:03:34 +0800

    add lock constant

--- src/main/java/com/webank/webase/node/mgr/lock/DbWeLock.java
+++ src/main/java/com/webank/webase/node/mgr/lock/DbWeLock.java
@@ -1,5 +1,6 @@
 package com.webank.webase.node.mgr.lock;
 
+import com.webank.webase.node.mgr.config.properties.ConstantProperties;
 import com.webank.webase.node.mgr.lock.entity.TbLock;
 import com.webank.webase.node.mgr.lock.service.LockService;
 import lombok.extern.log4j.Log4j2;
@@ -20,45 +21,41 @@ import java.util.concurrent.TimeUnit;
 @Log4j2
 public class DbWeLock implements WeLock {
     /**
-     * 将requestid保存在该变量中
+     * Save the thread ID in this variable
      */
-    private final static ThreadLocal<String> requestIdTL = new ThreadLocal<>();
-    /**
-     * 获取锁的超时时间，这个时间内获取不到将重试
-     */
-    private final static int GET_LOCK_TIME_OUT = 3000;
+    private final static ThreadLocal<String> threadIdTL = new ThreadLocal<>();
     @Autowired
     private LockService lockService;
-
+    @Autowired
+    private ConstantProperties constantProperties;
 
     /**
-     * 获取锁
+     * get Lock
      *
-     * @param lockKey 锁key
+     * @param lockKey lock key
      * @return
      **/
     @Override
     public boolean getLock(String lockKey) throws Exception {
-        return getLock(lockKey, GET_LOCK_TIME_OUT);
+        return getLock(lockKey, constantProperties.getLockTimeOut());
     }
 
     /**
      * 获取锁
      *
-     * @param lockKey         锁key
-     * @param lockTimeOut(毫秒) 持有锁的有效时间，防止死锁
+     * @param lockKey         lock key
+     * @param lockTimeOut(毫秒) Effective time of holding lock to prevent deadlock
      * @return
      **/
     @Override
     public boolean getLock(String lockKey, long lockTimeOut) throws Exception {
         boolean lockResult = false;
-        String requestId = getRequestId();
+        String requestId =this.getThreadId();
         long startTime = System.currentTimeMillis();
         while (true) {
             TbLock lockEntity = lockService.getLock(lockKey);
             if (Objects.isNull(lockEntity)) {
-                //插入一条记录,重新尝试获取锁
-                String reqId = this.getRequestId();
+                String reqId = this.getThreadId();
                 int add = lockService.add(TbLock.builder().lockKey(lockKey).threadId(reqId).lockCount(1)
                         .timeout(System.currentTimeMillis() + lockTimeOut).version(0).build());
                 if (add == 1) {
@@ -66,9 +63,9 @@ public class DbWeLock implements WeLock {
                     break;
                 }
             } else {
-                String reqId = lockEntity.getThreadId();
-                //如果reqid为空字符，表示锁未被占用
-                if ("".equals(reqId)) {
+                String threadId = lockEntity.getThreadId();
+                //If ThreadID is empty, it indicates that the lock is not occupied
+                if ("".equals(threadId)) {
                     lockEntity.setThreadId(requestId);
                     lockEntity.setLockCount(1);
                     lockEntity.setTimeout(System.currentTimeMillis() + lockTimeOut);
@@ -76,8 +73,9 @@ public class DbWeLock implements WeLock {
                         lockResult = true;
                         break;
                     }
-                } else if (requestId.equals(reqId)) {
-                    //如果request_id和表中request_id一样表示锁被当前线程持有者，此时需要加重入锁
+                } else if (requestId.equals(threadId)) {
+                    //If request_ ID and request in the table_ The same as ID indicates that the
+                    // lock is held by the current thread, and the lock needs to be added
                     lockEntity.setTimeout(System.currentTimeMillis() + lockTimeOut);
                     lockEntity.setLockCount(lockEntity.getLockCount() + 1);
                     if (lockService.update(lockEntity) == 1) {
@@ -85,13 +83,13 @@ public class DbWeLock implements WeLock {
                         break;
                     }
                 } else {
-                    //锁不是自己的，并且已经超时了，则重置锁，继续重试
+                    //If the lock is not own and has timed out, reset the lock and continue to try again
                     if (lockEntity.getTimeout() < System.currentTimeMillis()) {
                         this.resetLock(lockEntity);
                     } else {
-                        //如果未超时，休眠100毫秒，继续重试
-                        if (startTime + GET_LOCK_TIME_OUT > System.currentTimeMillis()) {
-                            TimeUnit.MILLISECONDS.sleep(10000);
+                        //If it does not time out, sleep for 100 milliseconds and continue to try again
+                        if (startTime + constantProperties.getGetLockTimeOut() > System.currentTimeMillis()) {
+                            TimeUnit.MILLISECONDS.sleep(100);
                         } else {
                             break;
                         }
@@ -103,7 +101,7 @@ public class DbWeLock implements WeLock {
     }
 
     /**
-     * 释放锁
+     * unlock
      *
      * @param lockKey
      * @throws Exception
@@ -111,7 +109,7 @@ public class DbWeLock implements WeLock {
     @Override
     public void unlock(String lockKey) throws Exception {
         //获取当前线程requestId
-        String requestId = this.getRequestId();
+        String requestId = this.getThreadId();
         TbLock lockEntity = lockService.getLock(lockKey);
         //当前线程requestId和库中request_id一致 && lock_count>0，表示可以释放锁
         if (Objects.nonNull(lockEntity) && requestId.equals(lockEntity.getThreadId()) && lockEntity.getLockCount() > 0) {
@@ -126,18 +124,17 @@ public class DbWeLock implements WeLock {
     }
 
     /**
-     * 获取当前线程requestid
+     * Get the current thread thread id
      *
      * @return
      **/
-    private String getRequestId() {
-        String requestId = requestIdTL.get();
-        if (StringUtils.isBlank(requestId)) {
-            requestId = UUID.randomUUID().toString();
-            requestIdTL.set(requestId);
+    private String getThreadId() {
+        String threadId = threadIdTL.get();
+        if (StringUtils.isBlank(threadId)) {
+            threadId = UUID.randomUUID().toString();
+            threadIdTL.set(threadId);
         }
-        log.debug("requestId:{}", requestId);
-        return requestId;
+        return threadId;
     }
 
     /**
